a6b1e1d2580c79c301f124d98056d2f2c26d1bda
Adam Duro <adam@duromedia.com>
2013-08-31 18:09:45 -0700

    Moving back to json parse method of creating package.json

--- app/index.js
+++ app/index.js
@@ -73,6 +73,18 @@ ClutchGenerator.prototype.fetchBoilerplate = function fetchBoilerplate() {
 
 ClutchGenerator.prototype.buildSkeleton = function buildSkeleton() {
   var self = this;
-  this.directory('.','.');
+
+  this.directory('.','.', function(data, filePath, filename){
+    // Transform the package.json
+    if (filePath == path.join(self.sourceRoot(), 'package.json')) {
+      data              = JSON.parse(data);
+      data.name         = self.appName;
+      data.author       = self.realname;
+      data.description  = self.appDescription;
+      data.version      = self.version;
+      return JSON.stringify(data, null, 2);
+    }
+    return data;
+  });
 
 };
